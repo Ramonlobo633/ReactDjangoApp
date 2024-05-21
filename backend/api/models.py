141c1149f7b5b016d5935a5b73dcb5697025b375
Ramonlobo633 <ramonlobo633@gmail.com>
2024-05-20 21:55:40 -0300

    push backend & frontend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    auth = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+
+    def __str__(self) -> str:
+        return self.title
+
